73fe9b98a84ded61b6e3b350a8e6c3f04033cca3
Yordi Kalef Miranda Hernandez <sisykmirandah@centrotlalocan.edu.mx>
2025-12-01 10:24:57 -0600

    Actualizacion archivo db
    Correcciones

--- Yordi Kalef Miranda/asistencias_api/db.php
+++ Yordi Kalef Miranda/asistencias_api/db.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-$host = "localhost"; // o "192.168.3.8" si lo usas desde otra PC
+$host = "localhost";
 $port = "3306";
 $db   = "Asistencias";
 $user = "root";
